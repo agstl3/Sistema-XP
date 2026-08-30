8c76cff7230963f52793a86dd6749138329a4fb6
agstl3 <sv8kv98psz@privaterelay.appleid.com>
2026-08-30 01:21:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -141,8 +141,9 @@ async function saveStudentToSupabase(student) {
 
 // --- NAVEGAÇÃO ENTRE ABAS ---
 function switchTab(tabId, button) {
-  document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
-  document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
+alert('O botão está funcionando!');
+document.querySelectorAll('.tab-content').forEach(tab => tab.classList.remove('active'));
+document.querySelectorAll('.tab-btn').forEach(btn => btn.classList.remove('active'));
   
   document.getElementById(tabId).classList.add('active');
   button.classList.add('active');
